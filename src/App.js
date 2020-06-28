860e7a59f196a78ad3fee54daa3356b02ca144a3
Chase Graffeo <chasegraffeo@gmail.com>
2020-06-28 09:26:32 -0500

    d

--- src/App.js
+++ src/App.js
@@ -19,28 +19,6 @@ function Home() {
     <br></br>
     <p className="d-flex justify-content-center text-warning">My name is Chase Graffeo I'm a Fullstack Software Developer and single Father.<br></br> I enjoy spending time with my son, video games, anime, traveling and all food. </p>
 </Container>
-<br></br>
- <Container className="main d-flex justify-content-center">
- <Card className="card" style={{ width: 600 }}>
-     <Card.Body className=" d-flex justify-content-center text-warning bg-secondary">
-         <AiFillLinkedin size="30px" />
-         <a href="https://www.linkedin.com/in/chasegraffeo/" className="d-flex justify-content-center text-warning" >
-             <h4>My Linkedin</h4>
-         </a>
-     </Card.Body>
- </Card>
-</Container>
-<br></br>
-     <Container className="main d-flex justify-content-center">
-     <Card className="card" style={{ width: 600 }}>
-       <Card.Body className="card-body d-flex justify-content-center text-warning bg-secondary">
-         <AiFillGithub size="30px" />
-         <a href="https://github.com/chasegraffeo" className="d-flex justify-content-center text-warning">
-           <h4>Visit my Github</h4>
-         </a>
-       </Card.Body>
-     </Card>
-   </Container>
    <br></br>
 <Container className="main d-flex justify-content-center">
       <Card className="card" style={{ width: 600 }}>
@@ -63,6 +41,28 @@ function Home() {
      </Card.Body>
    </Card>
  </Container>
+<br></br>
+ <Container className="main d-flex justify-content-center">
+ <Card className="card" style={{ width: 600 }}>
+     <Card.Body className=" d-flex justify-content-center text-warning bg-secondary">
+         <AiFillLinkedin size="30px" />
+         <a href="https://www.linkedin.com/in/chasegraffeo/" className="d-flex justify-content-center text-warning" >
+             <h4>My Linkedin</h4>
+         </a>
+     </Card.Body>
+ </Card>
+</Container>
+<br></br>
+     <Container className="main d-flex justify-content-center">
+     <Card className="card" style={{ width: 600 }}>
+       <Card.Body className="card-body d-flex justify-content-center text-warning bg-secondary">
+         <AiFillGithub size="30px" />
+         <a href="https://github.com/chasegraffeo" className="d-flex justify-content-center text-warning">
+           <h4>Visit my Github</h4>
+         </a>
+       </Card.Body>
+     </Card>
+   </Container>
  <br></br>
 <Container className="main d-flex justify-content-center">
       <Card className="card" style={{ width: 600 }}>
